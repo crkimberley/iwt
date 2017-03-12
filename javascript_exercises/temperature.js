d7175a17ae123558c98501e3e6953b143d95e1ac
Chris Kimberley <chris@chriskimberley.com>
2017-03-12 12:37:05 +0000

    Javascript exercises: temperature conversion complete...now with css added

---
+++ javascript_exercises/temperature.js
@@ -0,0 +1,7 @@
+/**
+ * Created by chris on 11/03/2017.
+ */
+function celsius(fahrenheit) {return (fahrenheit - 32.0) * 5.0 / 9.0}
+
+function fahrenheit(celsius) {return celsius * 9.0 / 5.0 + 32.0}
+
