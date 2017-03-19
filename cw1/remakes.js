94c7f888eb5d93f1016d8fd9a8930a579aa459ce
Chris Kimberley <chris@chriskimberley.com>
2017-03-19 01:20:25 +0000

    cw1: part 2(a) code tidied up a little

--- cw1/remakes.js
+++ cw1/remakes.js
@@ -8,26 +8,32 @@ function getXML(myUrl) {
 }
 
 function remakesTable() {
-    $('#tableLocation').empty();
+    $('#results').empty();
     var xmlDoc = getXML("remakes.xml");
     var stylesheet = getXML("remakes.xsl");
+
     var title = document.getElementById("remakeTitle").value;
     var year = document.getElementById("remakeYear").value;
     var fraction = document.getElementById('fraction').value;
+
     var titlePredicate = title ? "rtitle = '" + title + "'" : "*";
+
     var yearOperator = $('input[type=radio][name=year]:checked').val();
     var yearOp = yearOperator === "equals" ? "=" : (yearOperator === "lessThan" ? "<" : ">");
     var yearPredicate = year ? "ryear " + yearOp + " '" + year + "'" : "*";
+
     var fractionOperator = $('input[type=radio][name=fraction]:checked').val();
     var fractionOp = fractionOperator === "equals" ? "=" : (fractionOperator === "lessThan" ? "<" : ">");
     var fractionPredicate = fraction ? "fraction " + fractionOp + " '" + fraction + "'" : "*";
+
     var selection = "remake[" + titlePredicate + " and " + yearPredicate + " and " + fractionPredicate + "]";
     $(stylesheet).find("xsl\\:for-each, for-each").attr("select", selection);
+    
     if (typeof (XSLTProcessor) != "undefined") {
         var processor = new XSLTProcessor();
         processor.importStylesheet(stylesheet);
         var result = processor.transformToFragment(xmlDoc, document);
-        document.getElementById("tableLocation").appendChild(result);
+        document.getElementById("results").appendChild(result);
     } else
         window.alert("Your browser does not support the XSLTProcessor object");
 }
